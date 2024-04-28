c96bdd5a02a21fbf85f5bed62da55f9f05246ec3
Riri <Daydreamerriri@outlook.com>
2024-04-28 11:46:02 +0800

    fix: style/indent for jsx

--- src/configs/all.ts
+++ src/configs/all.ts
@@ -1,6 +1,6 @@
-import type { FlatConfigItem } from '@antfu/eslint-config'
+import type { TypedFlatConfigItem } from '@antfu/eslint-config'
 
-export async function all(): Promise<FlatConfigItem[]> {
+export async function all(): Promise<TypedFlatConfigItem[]> {
   return [
     {
       name: 'ririd:all',
@@ -28,6 +28,37 @@ export async function all(): Promise<FlatConfigItem[]> {
             argsIgnorePattern: '^_',
           },
         ],
+        '@stylistic/indent': ['error', 2, {
+          ArrayExpression: 1,
+          CallExpression: {
+            arguments: 1,
+          },
+          flatTernaryExpressions: false,
+          FunctionDeclaration: {
+            body: 1,
+            parameters: 1,
+          },
+          FunctionExpression: {
+            body: 1,
+            parameters: 1,
+          },
+          ignoreComments: false,
+          ignoredNodes: [
+            'TemplateLiteral *',
+            'TSIntersectionType',
+            'TSTypeParameterInstantiation',
+            'FunctionExpression > .params[decorators.length > 0]',
+            'FunctionExpression > .params > :matches(Decorator, :not(:first-child))',
+            'ClassBody.body > PropertyDefinition[decorators.length > 0] > .key',
+          ],
+          ImportDeclaration: 1,
+          MemberExpression: 1,
+          ObjectExpression: 1,
+          offsetTernaryExpressions: true,
+          outerIIFEBody: 1,
+          SwitchCase: 1,
+          VariableDeclarator: 1,
+        }],
       },
     },
   ]
--- src/utils.ts
+++
@@ -1,6 +0,0 @@
-import type { Awaitable, UserConfigItem } from '@antfu/eslint-config'
-
-export async function combine(...configs: Awaitable<UserConfigItem | UserConfigItem[]>[]): Promise<UserConfigItem[]> {
-  const resolved = await Promise.all(configs)
-  return resolved.flat()
-}
